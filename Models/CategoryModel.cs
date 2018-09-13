bca209f150f00c2fd2f86f166bd6ca5a052c384d
stp5940 <sitthipong114@gmail.com>
2018-09-14 01:24:33 +0700

    Update CodeDemo

---
+++ Models/CategoryModel.cs
@@ -0,0 +1,9 @@
+namespace www.Models
+{
+    public class CategoryModel
+    {
+        public int ID { get; set;}
+        public string Name { get; set;}
+        public string Description { get; set;}
+    }
+}
